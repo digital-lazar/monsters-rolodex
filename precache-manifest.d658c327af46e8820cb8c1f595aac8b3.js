bf67fec197566bad65ac4b9d795fbcb637b10d91
Lazar <lupumarketing@gmail.com>
2019-09-08 15:11:23 +0200

    Updates

---
+++ precache-manifest.d658c327af46e8820cb8c1f595aac8b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16baba0d4675dbfedf4c8e045e8f3f11",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d039cf47161cbb33de07",
+    "url": "/monsters-rolodex/static/css/main.14ba4993.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "d039cf47161cbb33de07",
+    "url": "/monsters-rolodex/static/js/main.76776ac0.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
